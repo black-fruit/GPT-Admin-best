fe03c95701d8c47eb14b85fdbeccf58f6162eb6c
pailecheng <981005580@qq.com>
2023-05-25 17:32:04 +0800

    0056

--- src/views/chat/hooks/useUsingContext.ts
+++ src/views/chat/hooks/useUsingContext.ts
@@ -9,7 +9,7 @@ export function useUsingContext() {
   const usingContext = computed<boolean>(() => chatStore.usingContext)
 
   function toggleUsingContext() {
-    chatStore.setUsingContext(!usingContext.value)
+    chatStore.setUsingContext(!usingContext.value,2356)
     if (usingContext.value)
       ms.success(t('chat.turnOnContext'))
     else
