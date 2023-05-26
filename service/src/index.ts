3075a59e5ba582e50c7833250596415309a359f2
pailecheng <981005580@qq.com>
2023-05-26 10:00:48 +0800

    9999

--- service/src/index.ts
+++ service/src/index.ts
@@ -555,7 +555,7 @@ router.post('/config', rootAuth, async (req, res) => {
 router.post('/session', async (req, res) => {
   try {    
     const config = await getCacheConfig()
-    console.log(config.siteConfig);    
+    console.log(config);    
     const hasAuth = config.siteConfig.loginEnabled  
     const allowRegister = (await getCacheConfig()).siteConfig.registerEnabled
     if (config.apiModel !== 'ChatGPTAPI' && config.apiModel !== 'ChatGPTUnofficialProxyAPI')
