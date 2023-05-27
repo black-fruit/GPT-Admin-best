5693923b5b0ab2e746aaedc6f62932a1fdb90487
pailecheng <981005580@qq.com>
2023-05-27 17:18:03 +0800

    0009

--- service/src/index.ts
+++ service/src/index.ts
@@ -482,6 +482,7 @@ router.post('/chat-process', [auth, limiter], async (req, res) => {
 })
 
 router.post('/user-register', async (req, res) => {
+  console.log('222323');
   try {
     const { username, password } = req.body as { username: string; password: string }
     const config = await getCacheConfig()
