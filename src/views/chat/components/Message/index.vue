80c0c66b23a2219a3283340ecbbf2263dece384f
pailecheng <981005580@qq.com>
2023-06-09 11:01:42 +0800

    6666

--- src/views/chat/components/Message/index.vue
+++ src/views/chat/components/Message/index.vue
@@ -181,7 +181,7 @@ async function handlePreviousResponse(next: number) {
           :as-raw-text="asRawText"
         />
         <NImage
-          width="100px"
+          object-fit="contain"
           :inversion="inversion"
           src="https://camo.githubusercontent.com/757c6919b6d049ef5c18f31be6d957abea9b7446e73dc1c87dbd1eef6f1e14bc/68747470733a2f2f692e6c6f6c692e6e65742f323031382f31312f30342f356264663066626162383663652e706e67"
          />
