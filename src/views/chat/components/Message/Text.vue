e790e297e73c2340da45b1cfc8d48a1161d24f6c
pailecheng <981005580@qq.com>
2023-06-14 17:19:48 +0800

    3333

--- src/views/chat/components/Message/Text.vue
+++ src/views/chat/components/Message/Text.vue
@@ -100,7 +100,7 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div class="text-black" :class="wrapClass">
+  <div class="text-black" :class="isurl ? '' : wrapClass">
     <div ref="textRef" class="leading-relaxed break-words">
       <div v-if="!inversion" class="flex items-end">
         <div v-if="!asRawText" class="w-full markdown-body" v-html="text" />
